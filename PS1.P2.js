646c71276600a7bf021a53fcb075487ad110ce72
Kevin Tu <57199141+kevintu2@users.noreply.github.com>
2021-09-24 21:30:52 -0400

    problem 2

---
+++ PS1.P2.js
@@ -0,0 +1,41 @@
+const evaluate = input =>{
+    var a = Number(input.charAt(0));
+    var b = Number(input.charAt(2));
+
+    if(input.charAt(1).localeCompare("+") == 0){
+        return add(a,b);
+    }
+    else if(input.charAt(1).localeCompare("-") == 0){
+        return minus(a,b);
+    }
+    else if(input.charAt(1).localeCompare("*") == 0){
+        return multi(a,b);
+    }
+    else if(input.charAt(1).localeCompare("/") == 0){
+        return div(a,b);
+    }
+    else if(input.charAt(1).localeCompare("^") == 0){
+        return pow(a,b);
+    }
+    else{
+        return "No valid Expression";
+    }
+
+}
+const add = (a,b) =>{
+    return a + b;
+}
+const minus = (a,b) =>{
+    return a - b;
+}
+const multi = (a,b) =>{
+    return a * b;
+}
+const div = (a,b) =>{
+    return a/b;
+}
+const pow = (a,b) =>{
+    return Math.pow(a,b);
+}
+
+console.log(evaluate("8^2"));
